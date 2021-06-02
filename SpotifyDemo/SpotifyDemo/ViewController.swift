a16ad9b0521d9e74e562bdbbe29d8404fb66f818
jatinfl15 <70931173+jatinfl15@users.noreply.github.com>
2021-06-02 10:10:46 +0530

    Add files via upload

---
+++ SpotifyDemo/SpotifyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyDemo
+//
+//  Created by Martis on 02/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
